8e252d5e43e5543f4bd1fc209ff68f0fdf7d6b67
DKFAEL <rafaelnunesemp@hotmail.com>
2023-11-28 15:53:07 -0300

    First commit

---
+++ lab-peti9/src/main/java/com/example/labpeti9/LabPeti9Application.java
@@ -0,0 +1,13 @@
+package com.example.labpeti9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPeti9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPeti9Application.class, args);
+	}
+
+}
---
+++ lab-peti9/src/test/java/com/example/labpeti9/LabPeti9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labpeti9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabPeti9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
